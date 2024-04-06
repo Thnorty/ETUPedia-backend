34ee1747d1d757ff4b06e845c692fab485580f7b
Thnorty <ototatik@gmail.com>
2024-04-06 16:21:45 +0300

    added get-teacher-info url and fixed some things

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('get-students/', views.GetStudentsApiView.as_view(), name='students'),
     path('get-student-info/', views.GetStudentInfoApiView.as_view(), name='student_info'),
+
+    path('get-teachers/', views.GetTeachersApiView.as_view(), name='teachers'),
+    path('get-teacher-info/', views.GetTeacherInfoApiView.as_view(), name='teacher_info'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,14 +1,14 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
-from api.models import Student, Teacher, Classroom, Lesson, LessonSection, LessonSectionStudent, LessonSectionClassroom
+from api.models import Student, Teacher, Classroom, Lesson, LessonSection, LessonSectionStudent, \
+    LessonSectionClassroom, LessonSectionTeacher
 
 
 # Create your views here.
-
-
 class GetStudentsApiView(APIView):
-    def get(self, request):
+    @staticmethod
+    def get(request):
         students = Student.objects.all().order_by('name')
         response = []
         for student in students:
@@ -24,7 +24,8 @@ class GetStudentsApiView(APIView):
 
 
 class GetStudentInfoApiView(APIView):
-    def post(self, request):
+    @staticmethod
+    def post(request):
         student_id = request.data['student_id']
         student = Student.objects.get(id=student_id)
         student_lesson_sections = LessonSectionStudent.objects.filter(student=student)
@@ -53,8 +54,9 @@ class GetStudentInfoApiView(APIView):
 
 
 class GetTeachersApiView(APIView):
-    def get(self, request):
-        teachers = Teacher.objects.all()
+    @staticmethod
+    def get(request):
+        teachers = Teacher.objects.all().order_by('name')
         response = []
         for teacher in teachers:
             response.append({
@@ -63,8 +65,34 @@ class GetTeachersApiView(APIView):
         return JsonResponse(response, safe=False)
 
 
+class GetTeacherInfoApiView(APIView):
+    @staticmethod
+    def post(request):
+        teacher_name = request.data['teacher_name']
+        teacher = Teacher.objects.get(name=teacher_name)
+        teacher_lesson_sections = LessonSectionTeacher.objects.filter(teacher_name=teacher)
+        lesson_sections = []
+        for section in teacher_lesson_sections:
+            lesson_section_classrooms = LessonSectionClassroom.objects.filter(lesson_section=section.lesson_section)
+            classrooms_and_times = [
+                {'classroom': lsc.classroom_name.name, 'time': lsc.time} for lsc in lesson_section_classrooms
+            ]
+            lesson_sections.append({
+                'lesson_code': section.lesson_section.lesson_code.lesson_code,
+                'lesson_name': section.lesson_section.lesson_code.name,
+                'lesson_section_number': section.lesson_section.lesson_section_number,
+                'classrooms_and_times': classrooms_and_times
+            })
+        response = {
+            'name': teacher.name,
+            'lesson_sections': lesson_sections
+        }
+        return JsonResponse(response, safe=False)
+
+
 class GetClassroomsApiView(APIView):
-    def get(self, request):
+    @staticmethod
+    def get(request):
         classrooms = Classroom.objects.all()
         response = []
         for classroom in classrooms:
@@ -75,7 +103,8 @@ class GetClassroomsApiView(APIView):
 
 
 class GetLessonsApiView(APIView):
-    def get(self, request):
+    @staticmethod
+    def get(request):
         lessons = Lesson.objects.all()
         response = []
         for lesson in lessons:
@@ -87,7 +116,8 @@ class GetLessonsApiView(APIView):
 
 
 class GetLessonSectionsApiView(APIView):
-    def get(self, request):
+    @staticmethod
+    def get(request):
         lesson_sections = LessonSection.objects.all()
         response = []
         for lesson_section in lesson_sections:
