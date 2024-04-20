1256eb7dd5452bf2267d811c2b9e33c233c37f6c
Thnorty <ototatik@gmail.com>
2024-04-20 23:43:15 +0300

    added posts support

--- backend/settings.py
+++ backend/settings.py
@@ -43,8 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'api',
+    'posts',
 ]
 
 MIDDLEWARE = [
--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+
 from api.models import Profile
 
 
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -12,4 +14,4 @@ class Post(models.Model):
         ordering = ['-timestamp']
 
     def __str__(self):
-        return f'{self.author.name} - {self.content[:20]}'
+        return f'{self.author.student.name + self.author.student.surname} - {self.content[:20]}'
