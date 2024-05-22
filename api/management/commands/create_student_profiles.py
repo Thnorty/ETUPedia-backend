0eb6d1cd3f4577d0f122ed9f276c4f0846443911
Thnorty <ototatik@gmail.com>
2024-05-22 20:39:20 +0300

    updated create_student_profiles function

--- api/management/commands/create_student_profiles.py
+++ api/management/commands/create_student_profiles.py
@@ -8,14 +8,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         students = Student.objects.all()
+        total_students = students.count()
+        completed = 0
 
         for student in students:
+            completed += 1
             # Check if the student already has a profile
             if not Profile.objects.filter(student=student).exists():
                 # Create a new user for each student
-                user = User.objects.create_user(username=student.mail, password='defaultpassword')
+                user = User.objects.create_user(username=student.id, password='defaultpassword')
 
                 # Create a new profile for each student
                 Profile.objects.create(user=user, student=student)
+
+                # Print the progress
+                completed_percentage = (completed / total_students) * 100
+                self.stdout.write(self.style.SUCCESS(f'{completed_percentage:.2f}% - Profile created for {student.name} {student.surname}'))
 
         self.stdout.write(self.style.SUCCESS('Successfully created profiles for all students'))
