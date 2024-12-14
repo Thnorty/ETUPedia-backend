c6b0eb329fda83841ebf81e3c283b3785a4e5a46
Thnorty <ototatik@gmail.com>
2024-12-14 19:15:02 +0300

    added favicon to the admin panel

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from django.views.static import serve
+from backend import settings
 urlpatterns = [
+    re_path(r'^favicon\.ico$', serve, {'path': 'favicon.ico', 'document_root': settings.STATIC_URL}),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('posts/', include('posts.urls')),
